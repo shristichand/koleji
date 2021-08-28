2d8544cd2e00726d6a4a46978a70fb831ed0692a
itzkbp <itzkbp@gmail.com>
2021-08-28 06:52:26 +0545

    added support for no input

--- src/main.c
+++ src/main.c
@@ -1,4 +1,18 @@
-int main()
+#include "include/kol.h"
+#include "include/main.h"
+
+#ifndef INCLUDED_STDIO_H
+    #include <stdio.h>
+    #define INCLUDED_STDIO_H
+#endif
+
+int main(int argc, char *argv[])
 {
+    if(argc<2)
+    {
+        printf("No input file Specified...\n");
+        return 1;
+    }
+
     return 0;
 }
