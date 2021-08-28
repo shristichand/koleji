cc9ced9cc122d385647c7e29b2206b635d3f7a41
itzkbp <itzkbp@gmail.com>
2021-08-28 09:32:14 +0545

    lexer initialization

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -3,14 +3,36 @@
 
     #include "token.h"
 
+    #ifndef INCLUDED_STDIO_H
+        #include <stdio.h>
+        #define INCLUDED_STDIO_H
+    #endif
+
     typedef struct st_Lexer {
 
         char *src;
+        size_t src_size;
         char c;
         unsigned int i;
 
     } Lexer;
 
     Lexer *init_lexer(char *src);
-    
+
+    void lexer_advance(Lexer *lexer);
+
+    char lexer_peek(Lexer *lexer, int offset);
+
+    Token *lexer_advance_with(Lexer *lexer, Token *token);
+
+    Token *lexer_advance_current(Lexer *lexer, int type);
+
+    void lexer_skip_whitespace(Lexer *lexer);
+
+    Token *lexer_parse_id(Lexer *lexer);
+
+    Token *lexer_parse_number(Lexer *lexer);
+
+    Token *lexer_next_token(Lexer *lexer);
+
 #endif
